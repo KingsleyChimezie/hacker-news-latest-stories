fe67284acff9e4b8b6fccbcd5962f55d1d9b359c
Kingsley Chimezie <kc@kingsley.tech>
2022-08-07 17:37:55 +0100

    Added new Prime NG modules - for designing Hacker News feed.

--- frontend-angular/src/app/modules/primeng/primeng.module.ts
+++ frontend-angular/src/app/modules/primeng/primeng.module.ts
@@ -1,8 +1,23 @@
 import { NgModule } from '@angular/core';
 import {ProgressSpinnerModule} from 'primeng/progressspinner';
+import {DataViewModule} from 'primeng/dataview';
+import {ButtonModule} from 'primeng/button';
+import {PanelModule} from 'primeng/panel';
+import {DropdownModule} from 'primeng/dropdown';
+import {DialogModule} from 'primeng/dialog';
+import {InputTextModule} from 'primeng/inputtext';
+import {RatingModule} from 'primeng/rating';
+import {RippleModule} from 'primeng/ripple';
 
 const PrimengComponents = [
-  ProgressSpinnerModule
+  ProgressSpinnerModule,
+  DataViewModule,
+  PanelModule,
+  DropdownModule,
+  DialogModule,
+  InputTextModule,
+  RatingModule,
+  RippleModule
 ]
 
 @NgModule({
