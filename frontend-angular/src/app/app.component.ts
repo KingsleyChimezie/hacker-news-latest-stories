5c9f29a2d3721542594cea7bb09e09b3d0775b5e
Kingsley Chimezie <kc@kingsley.tech>
2022-08-07 20:59:56 +0100

    Created 404 routing functionality

--- frontend-angular/src/app/app.component.ts
+++ frontend-angular/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'frontend-angular';
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+    // Route Error 404
+    this.router.errorHandler = (error: any) => {
+      const routerError = error.toString();
+      if (routerError.indexOf('Cannot match any routes') >= 0) {
+        this.router.navigate(['/404']);
+      } else {
+        throw error;
+      }
+    };
+  }
 }
