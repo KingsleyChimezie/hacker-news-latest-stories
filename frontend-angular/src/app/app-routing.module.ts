9ab0ee91bef7a41447a9d18668928c5dcbc4ebf7
Kingsley Chimezie <kc@kingsley.tech>
2022-08-07 20:58:37 +0100

    Setting up routing

--- frontend-angular/src/app/app-routing.module.ts
+++ frontend-angular/src/app/app-routing.module.ts
@@ -1,10 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+// ERROR PAGES
+import { Error404Component } from './pages/error-pages/error404/error404.component';
+
+// GENERAL PAGES
+import { AngularComponent } from './pages/angular/angular.component';
+import { HnNewstoriesComponent } from './pages/hn-newstories/hn-newstories.component';
+
+
+const routes: Routes = [
+  { path: '404', component: Error404Component, title: '404 - Page Not Found' },
+  { path: '', component: HnNewstoriesComponent, title: 'HN - Newest Stories' },
+  { path: 'home', component: HnNewstoriesComponent, title: 'HN - Newest Stories' },
+  { path: 'new', component: HnNewstoriesComponent, title: 'HN - Newest Stories' },
+  { path: 'angular', component: AngularComponent, title: 'Angular Template' }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    scrollPositionRestoration: 'enabled',
+    anchorScrolling: 'enabled',
+    onSameUrlNavigation: 'reload'
+   })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
