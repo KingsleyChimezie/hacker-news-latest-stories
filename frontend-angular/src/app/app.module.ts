e6a35b21cb3ffa337401b4890b9946a8d306371d
Kingsley Chimezie <kc@kingsley.tech>
2022-08-07 18:44:39 +0100

    Created shared components: nav and footer (2)

--- frontend-angular/src/app/app.module.ts
+++ frontend-angular/src/app/app.module.ts
@@ -4,16 +4,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-// Third Party Modules
+// THIRD PARTY MODULES
 import { PrimengModule } from './modules/primeng/primeng.module';
+
+// PAGES
 import { AngularComponent } from './pages/angular/angular.component';
 import { HnNewstoriesComponent } from './pages/hn-newstories/hn-newstories.component';
+
+// SHARED COMPONENTS
+import { NavComponent } from './shared/nav/nav.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AngularComponent,
-    HnNewstoriesComponent
+    HnNewstoriesComponent,
+    NavComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
