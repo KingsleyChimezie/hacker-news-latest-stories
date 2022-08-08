8026dbc4337425c9bad096dbc5ae91a9c88cc6c9
Kingsley Chimezie <kc@kingsley.tech>
2022-08-08 03:30:33 +0100

    Created nav and footer hiding feature based on current URL path

--- frontend-angular/src/app/shared/footer/footer.component.ts
+++ frontend-angular/src/app/shared/footer/footer.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
@@ -6,8 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
+  currentPath = '';
+  hide = true;
+  hidePathsArr = ['/angular'];
 
-  constructor() { }
+  constructor(
+    private location: Location,
+    private router: Router
+  ) {
+    // check current path in array
+    router.events.subscribe(() => {
+      this.currentPath = location.path();
+      this.hide = this.hidePathsArr.includes(this.currentPath);
+    });
+  }
 
   ngOnInit(): void {
   }
--- frontend-angular/src/app/shared/nav/nav.component.ts
+++ frontend-angular/src/app/shared/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -6,8 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav.component.scss']
 })
 export class NavComponent implements OnInit {
+  currentPath = '';
+  hide = true;
+  hidePathsArr = ['/angular'];
 
-  constructor() { }
+  constructor(
+    private location: Location,
+    private router: Router
+  ) {
+    // check current path in array
+    router.events.subscribe(() => {
+      this.currentPath = location.path();
+      this.hide = this.hidePathsArr.includes(this.currentPath);
+    });
+  }
 
   ngOnInit(): void {
   }
