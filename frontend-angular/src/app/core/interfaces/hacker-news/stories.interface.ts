22cc80931cd67594e31758639aaeb6e25b8c99c8
Kingsley Chimezie <kc@kingsley.tech>
2022-08-09 13:10:06 +0100

    Added more attributes to interface

--- frontend-angular/src/app/core/interfaces/hacker-news/stories.interface.ts
+++ frontend-angular/src/app/core/interfaces/hacker-news/stories.interface.ts
@@ -1,8 +1,11 @@
 export interface Story {
+  id: number;
   title: string;
   url: string;
   by: string;
   time: number;
   score: number;
   decendants: number;
+  type: string;
+  kids: number[];
 }
